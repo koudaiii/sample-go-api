6f7092c115923704b85a7a5a7a1a17d1a3d1190d
koudaiii <cs006061@gmail.com>
2017-09-19 12:17:16 +0900

    Add performance script

---
+++ script/performance/README.md
@@ -0,0 +1,18 @@
+Usage
+=====
+
+Requisites
+----------
+
+```sh
+$ cd script/performance
+$ go get github.com/icrowley/fake
+$ go run gen.go
+```
+
+Run
+---
+
+```sh
+$ ./script/performance/bench -n 10000 -c 4000
+```
---
+++ script/performance/gen.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+	"strconv"
+
+	"github.com/icrowley/fake"
+)
+
+type Entry struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		dummyEntries := &Entry{
+			Name:  fake.FullName(),
+			Email: fake.EmailAddress(),
+		}
+
+		f, err := os.Create("./test-" + strconv.Itoa(i) + ".json")
+		if err != nil {
+			panic(err)
+		}
+
+		bytes, err := json.Marshal(dummyEntries)
+		if err != nil {
+			panic(err)
+		}
+
+		if _, err := f.Write(bytes); err != nil {
+			panic(err)
+		}
+	}
+}
